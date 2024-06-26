f76d486b9240cd4234ac790d8a2389c6861ea2cf
Ngocdzung Smith <hysinh1@gmail.com>
2024-06-26 17:51:14 +0000

    Adds Holidays table to db via crud py file

--- sql-crud.py
+++ sql-crud.py
@@ -10,14 +10,27 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 # create a class-based model for the "Programmer" table
-class Programmer(base):
-    __tablename__ = "Programmer"
+# class Programmer(base):
+#     __tablename__ = "Programmer"
+#     id = Column(Integer, primary_key=True)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     gender = Column(String)
+#     nationality = Column(String)
+#     famous_for = Column(String)
+
+
+# create a class-based model for the "Programmer" table
+class Holidays(base):
+    __tablename__ = "Holidays"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
-    nationality = Column(String)
-    famous_for = Column(String)
+    country = Column(String)
+    city = Column(String)
+    type = Column(String)
+    favourite_attraction = Column(String)
+    favourite_food = Column(String)
+
+
 
 
 # instead of connecting to the database directly, we will ask for a session
@@ -30,92 +43,193 @@ session = Session()
 base.metadata.create_all(db)
 
 
+# Creating records in the Holidays table
+rome_holiday = Holidays(
+    country = "Italy",
+    city = "Rome",
+    type = "city",
+    favourite_attraction = "The Roman Forum",
+    favourite_food = "Calamari"
+)
+
+london_holiday = Holidays(
+    country = "United Kingdom",
+    city = "London",
+    type = "city",
+    favourite_attraction = "Museum of Natural History",
+    favourite_food = "Fish and Chips"
+)
+
+paris_holiday = Holidays(
+    country = "France",
+    city = "Paris",
+    type = "city",
+    favourite_attraction = "Catacombs",
+    favourite_food = "Crepes"
+)
+
+munich_holiday = Holidays(
+    country = "Germany",
+    city = "Munich",
+    type = "city",
+    favourite_attraction = "Deutsch Museum",
+    favourite_food = "Bratwurst"
+)
+
+prague_holiday = Holidays(
+    country = "Czech Republic",
+    city = "Prague",
+    type = "city",
+    favourite_attraction = "Prague Castle",
+    favourite_food = "Ice Cream"
+)
+
+
 # creating records on our Programmer table
-ada_lovelace = Programmer(
-    first_name = "Ada",
-    last_name = "Lovelace",
-    gender = "F",
-    nationality = "British",
-    famous_for = "First Programmer"
-)
+# ada_lovelace = Programmer(
+#     first_name = "Ada",
+#     last_name = "Lovelace",
+#     gender = "F",
+#     nationality = "British",
+#     famous_for = "First Programmer"
+# )
 
-alan_turing = Programmer(
-    first_name = "Alan",
-    last_name = "Turing",
-    gender = "M",
-    nationality = "British",
-    famous_for = "Modern Computing"
-)
+# alan_turing = Programmer(
+#     first_name = "Alan",
+#     last_name = "Turing",
+#     gender = "M",
+#     nationality = "British",
+#     famous_for = "Modern Computing"
+# )
 
-grace_hopper = Programmer(
-    first_name = "Grace",
-    last_name = "Hopper",
-    gender = "F",
-    nationality = "American",
-    famous_for = "COBOL language"
-)
+# grace_hopper = Programmer(
+#     first_name = "Grace",
+#     last_name = "Hopper",
+#     gender = "F",
+#     nationality = "American",
+#     famous_for = "COBOL language"
+# )
 
-grace_hopper = Programmer(
-    first_name = "Grace",
-    last_name = "Hopper",
-    gender = "F",
-    nationality = "American",
-    famous_for = "COBOL language"
-)
+# grace_hopper = Programmer(
+#     first_name = "Grace",
+#     last_name = "Hopper",
+#     gender = "F",
+#     nationality = "American",
+#     famous_for = "COBOL language"
+# )
 
-margaret_hamilton = Programmer(
-    first_name = "Margaret",
-    last_name = "Hamilton",
-    gender = "F",
-    nationality = "American",
-    famous_for = "Apollo 11"
-)
+# margaret_hamilton = Programmer(
+#     first_name = "Margaret",
+#     last_name = "Hamilton",
+#     gender = "F",
+#     nationality = "American",
+#     famous_for = "Apollo 11"
+# )
 
-bill_gates = Programmer(
-    first_name = "Bill",
-    last_name = "Gates",
-    gender = "M",
-    nationality = "American",
-    famous_for = "Microsoft"
-)
+# bill_gates = Programmer(
+#     first_name = "Bill",
+#     last_name = "Gates",
+#     gender = "M",
+#     nationality = "American",
+#     famous_for = "Microsoft"
+# )
 
-tim_berners_lee = Programmer(
-    first_name = "Tim",
-    last_name = "Berners-Lee",
-    gender = "M",
-    nationality = "British",
-    famous_for = "World Wide Web"
-)
+# tim_berners_lee = Programmer(
+#     first_name = "Tim",
+#     last_name = "Berners-Lee",
+#     gender = "M",
+#     nationality = "British",
+#     famous_for = "World Wide Web"
+# )
 
-ngocdzung_nguyen_smith = Programmer(
-    first_name = "NgocDzung",
-    last_name = "Nguyen Smith",
-    gender = "F",
-    nationality = "American",
-    famous_for = "Making Bad Code"
-)
+# ngocdzung_nguyen_smith = Programmer(
+#     first_name = "NgocDzung",
+#     last_name = "Nguyen Smith",
+#     gender = "F",
+#     nationality = "American",
+#     famous_for = "Making Bad Code"
+# )
 
-# add each instsance of our programmers to our session
+# add each instance of our programmers to our session
 # session.add(ada_lovelace)
 #session.add(alan_turing)
 #session.add(grace_hopper)
 #session.add(margaret_hamilton)
 #session.add(bill_gates)
 #session.add(tim_berners_lee)
-session.add(ngocdzung_nguyen_smith)
+# session.add(ngocdzung_nguyen_smith)
+session.add(rome_holiday)
+session.add(london_holiday)
+session.add(paris_holiday)
+session.add(prague_holiday)
+session.add(munich_holiday)
+
+
+
+# query the database to find all the Programmers
+# programmer = session.query(Programmer).filter_by(id=8).first()
+# programmer.famous_for = "World President"
 
 # commit our session to the database
 session.commit()
 
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# # deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure if you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+# else:
+#     print("No records found.")
+
+# DON'T USE THIS FOR DEMONSTRATION PURPOSES ONLY **************
+# delete multiple records 
+# Use defensive programming to ensure that you really want to delete all the records in the table
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+
 # query the database to find all Programmers
-programmers = session.query(Programmer)
-for programmer in programmers:
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     print(
+#         programmer.id,
+#         programmer.first_name + " " + programmer.last_name,
+#         programmer.gender,
+#         programmer.nationality,
+#         programmer.famous_for,
+#         sep=" | "
+#     )
+
+# query the database to find all Programmers
+holidays = session.query(Holidays)
+for holiday in holidays:
     print(
-        programmer.id,
-        programmer.first_name + " " + programmer.last_name,
-        programmer.gender,
-        programmer.nationality,
-        programmer.famous_for,
+        holiday.id,
+        holiday.country,
+        holiday.city,
+        holiday.type,
+        holiday.favourite_attraction,
+        holiday.favourite_food,
         sep=" | "
     )
 
